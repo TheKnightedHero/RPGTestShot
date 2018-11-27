4b6342a69a54fd177f9a2ebb5efff07dddbdd344
Caleb Herring <cherring1@harding.edu>
2018-11-27 15:13:48 -0600

    push updated class header

--- PlayerCharacter.h
+++ PlayerCharacter.h
@@ -29,13 +29,36 @@ class PlayerCharacter
         
         int maxHitPoints = 0;
         int currentHitPoints = 0;
-        int maxMana = 0;
-        int currentMana = 0;
-        int passivePerception = 10;
+        //int maxMana = 0;
+        //int currentMana = 0;
+        int passivePerception = 10; // if proficient with perception, should be 10 + perception
+        int totalExpEarned = 0;
 
-        //skills list
-        int animalHandling;
-        int athletics;
+        //skills list 
+        /*
+            - currently sorted Alphabetically
+            -I might sort them by attribute instead since the number is determined by 
+                attribute + proficiency if proficient in said skill.
+            -Also might be a little too ambitious for my first RPG since it is only supposed to be a simply CYA text game
+        */
+        int acrobatics = 0;
+        int animalHandling = 0;
+        int arcana = 0;
+        int athletics = 0;
+        int deception = 0;
+        int history = 0;
+        int insight = 0;
+        int intimidation = 0;
+        int investigation = 0;
+        int medicine = 0;
+        int nature = 0;
+        int perception = 0;
+        int performance = 0;
+        int persuasion = 0;
+        int religion = 0;
+        int sleightOfHand = 0;
+        int stealth = 0;
+        int survival = 0;
     
     public:
 
