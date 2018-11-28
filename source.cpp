89edd8aad78299e88dfb60f6fa62b237b1cb820a
Caleb Herring <cherring1@harding.edu>
2018-11-28 15:56:29 -0600

    push beginnings of main funtion

---
+++ source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "PlayerCharacter.h"
+#include "Armor.h"
+#include "Weapons.h"
+using namespace std;
+
+void main ()
+{
+    int selectedOption;
+    
+    cout << "----Main Menu----" << endl;
+    cout << "1) Create Character" << endl;
+    cout << "2) Purchase Equipment" << endl;
+    cout << "3) View Stats" << endl;
+    cout << "4) Enter Arena" << endl;
+    cout << "5) Exit Game" << endl;
+
+    cin >> selectedOption;
+    while(selectedOption != 5)
+    {
+
+    }
+}
