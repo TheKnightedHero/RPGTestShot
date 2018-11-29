dc2fe75e78b7b816bf0ec679a6df8a1ead5a98e8
Caleb Herring <cherring1@harding.edu>
2018-11-29 12:35:41 -0600

    push

--- source.cpp
+++ source.cpp
@@ -18,6 +18,31 @@ void main ()
     cin >> selectedOption;
     while(selectedOption != 5)
     {
+        if(selectedOption == 1)
+        {
 
+        }
+        else if(selectedOption == 2)
+        {
+
+        }
+        else if(selectedOption == 3)
+        {
+
+        }
+        else if(selectedOption == 4)
+        {
+
+        }
+
+        cout << "----Main Menu----" << endl;
+        cout << "1) Create Character" << endl;
+        cout << "2) Purchase Equipment" << endl;
+        cout << "3) View Stats" << endl;
+        cout << "4) Enter Arena" << endl;
+        cout << "5) Exit Game" << endl;
+
+        cin >> selectedOption;
     }
+    cout << "Thank you for playing." << endl;
 }
